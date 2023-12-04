30460dc518eddf75112c05e951ace808986f0539
Harrisson Bernal <harrisson.bernal@gmail.com>
2023-12-04 14:24:08 +0100

    Creando el Case de seleccion de productos

--- practica-ensaldas.sh
+++ practica-ensaldas.sh
@@ -4,9 +4,12 @@ SERVICES=( #esto es un arreglo
     frutas
     cafe
     papas
+    desayunos
 )
 
 CODE=1  #Codigo de seleccion del arreglo
+
+echo "Estos son los servicios que tengo para ti: "
 
 for v in "${SERVICES[@]}" #Recorriendo el arreglo
     do
@@ -15,4 +18,16 @@ for v in "${SERVICES[@]}" #Recorriendo el arreglo
 
     done
 
-echo "Tenemos fruta, Cafe y Papas"
+read -p "Cual servicio desea tener? : " CLIENT_SERVICES  #Con el -p se puede pregutar, remplaza el echo, Asigno el dato a la variable
+
+echo $CLIENT_SERVICES
+
+case $CLIENT_SERVICES in
+
+    1) echo "${SERVICES[$(expr $CLIENT_SERVICES - 1)]}";;
+    2) echo "${SERVICES[$(expr $CLIENT_SERVICES - 1)]}";;
+    3) echo "${SERVICES[$(expr $CLIENT_SERVICES - 1)]}";;
+    4) echo "${SERVICES[$(expr $CLIENT_SERVICES - 1)]}";;
+    *) echo "Este servicio no esta disponible";;
+
+esac
